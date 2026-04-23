924b85e1bba00d752e679bcf1834d5ab3c4b1511
Luis <seivaneluis@gmail.com>
2026-04-22 22:18:45 -0300

    Actualización de la Calculadora.

--- ejercicio_calculadora/ejercicio_calculadora.ino
+++ ejercicio_calculadora/ejercicio_calculadora.ino
@@ -1,45 +1,12 @@
 int a;
 int b;
+int opcion;
 
-void setup() {
-  Serial.begin(9600);
-  delay(1000);
-  Serial.println("Bienvenido a la Micro-calculadora");
-}
-
-void loop() {
-  Serial.println("Ingrese una opción:");
-  Serial.println("1. Sumar");
-  Serial.println("2. Restar");
-  Serial.println("3. Multiplicar");
-  Serial.println("4. Dividir");
-
-  while (Serial.available() == 0) {} // esperar entrada
-  int opcion = Serial.parseInt();
-
-  if (opcion >= 1 && opcion <= 4) {
-
-    Serial.println("Ingrese el primer número:");
-    delay(1500);
-    while (Serial.available() == 0) {}
-    a = Serial.parseInt();
-    delay(1500);
-    Serial.println("Ingrese el segundo número:");
-    while (Serial.available() == 0) {}
-    b = Serial.parseInt();
-
-    calculadora(a, b, opcion);
-
-  } else {
-    Serial.println("Opción inválida.");
-  }
-
-  delay(2000);
-}
+void calculadora(int a, int b, int dato);
 
 void calculadora(int a, int b, int dato) {
   String operacion;
-  int resultado = 0;
+  int resultado;
 
   if (dato == 1) {
     operacion = "suma";
@@ -67,4 +34,48 @@ void calculadora(int a, int b, int dato) {
   //Serial.print(operacion);
   //Serial.print(" es: ");
   //Serial.println(resultado);
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("Bienvenido a la Micro-calculadora");
+}
+
+void loop() {
+  Serial.println("1. Sumar");
+  Serial.println("2. Restar");
+  Serial.println("3. Multiplicar");
+  Serial.println("4. Dividir");
+  Serial.println("Ingrese una opción: ");
+
+  while (1) {
+  opcion = Serial.parseInt();
+
+  if (opcion >= 1 && opcion <= 4) {
+    Serial.println("Ingrese el primer número:");
+    Serial.read();
+    while (1){
+      a = Serial.parseInt();
+      if (a != 0){
+        Serial.println(a);
+        break;
+      }
+    }
+    Serial.println("Ingrese el segundo número:");
+    while (1){
+      b = Serial.parseInt();
+      if (b != 0){
+        Serial.println(b);
+        break;
+      }
+    }
+    break;
+  } else if (opcion < 1 && opcion > 4){
+    Serial.println("Opción inválida.");
+    Serial.read();
+  }
+  }
+  calculadora(a, b, opcion);
+  delay(2000);
 }
